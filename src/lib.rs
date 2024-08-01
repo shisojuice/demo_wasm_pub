a45cea5a1711d2ad33d4bd9e0cb4a9defcdf660e
shisojuice <167944658+shisojuice@users.noreply.github.com>
2024-08-01 15:41:51 +0900

    Revert "c"
    This reverts commit 7cfe133f54ab66835bd2330182cd705a6121511e.

--- src/lib.rs
+++ src/lib.rs
@@ -85,10 +85,10 @@ pub fn pixel_filter(mut buffer: Vec<u8>,canvas_width :u32,canvas_height :u32) ->
     let mut arr_chk = vec![0u8; width * height];
     for i in (0..buffer.len()).step_by(4) {
         let avg = (buffer[i] as u16 + buffer[i + 1] as u16 + buffer[i + 2] as u16) / 3;
-        arr_chk[i] = 1;
+        arr_chk[i] = avg as u8;//if avg < 1 { 1 } else { 0 };
     }
 
-    DataPackage::new(buffer, arr_chk)
+    DataPackage::new(Vec::from(buffer.as_slice()), arr_chk)
 }
 
 fn int_sqrt(n: u32) -> u32 {
