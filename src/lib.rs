a7e96fd9ce36bee0b287a9e4e69b1f7cc0b619c4
shisojuice <167944658+shisojuice@users.noreply.github.com>
2024-08-12 22:46:12 +0900

    c

--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,53 @@ pub fn pixel_filter(mut buffer: Vec<u8>,canvas_width :u32,canvas_height :u32) ->
     DataPackage::new(Vec::from(buffer.as_slice()), arr_chk,text)
 }
 
+
+#[wasm_bindgen]
+pub fn edge_black_filter(mut buffer: Vec<u8>,canvas_width :u32,canvas_height :u32) -> Vec<u8> {
+    let width = canvas_width as usize;
+    let height = canvas_height as usize;
+
+    // Sobel フィルタを使ってエッジ検出
+    let mut edges = vec![0u8; buffer.len() / 4];
+    for y in 1..(height - 1) {
+        for x in 1..(width - 1) {
+            let gx = -1 * buffer[((y - 1) * width + (x - 1)) * 4] as i16
+                + buffer[((y - 1) * width + (x + 1)) * 4] as i16
+                - 2 * buffer[((y) * width + (x - 1)) * 4] as i16
+                + 2 * buffer[((y) * width + (x + 1)) * 4] as i16
+                - buffer[((y + 1) * width + (x - 1)) * 4] as i16
+                + buffer[((y + 1) * width + (x + 1)) * 4] as i16;
+
+            let gy = buffer[((y - 1) * width + (x - 1)) * 4] as i16
+                + 2 * buffer[((y - 1) * width + (x)) * 4] as i16
+                + buffer[((y - 1) * width + (x + 1)) * 4] as i16
+                - buffer[((y + 1) * width + (x - 1)) * 4] as i16
+                - 2 * buffer[((y + 1) * width + (x)) * 4] as i16
+                - buffer[((y + 1) * width + (x + 1)) * 4] as i16;
+
+            let mag = int_sqrt((gx * gx + gy * gy) as u32).min(255) as u8;
+            edges[y * width + x] = mag;
+        }
+    }
+
+    // エッジを黒、それ以外を白に設定
+    for y in 0..height {
+        for x in 0..width {
+            let index = (y * width + x) * 4;
+            if edges[y * width + x] > 50 {
+                // エッジ
+                buffer[index] = 0;
+                buffer[index + 1] = 0;
+                buffer[index + 2] = 0;
+                // buffer[index + 3] = 255;
+            }
+        }
+    }
+
+    buffer
+}
+
+
 fn int_sqrt(n: u32) -> u32 {
     let mut x = n;
     let mut y = (x + 1) >> 1;
